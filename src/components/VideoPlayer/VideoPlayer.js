7fe79914a75dddfc66fa3b25b7fb881083245d1d
Calum_Muir <mulacrium@gmail.com>
2020-11-29 10:18:15 +0000

    Added video loading message

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,16 +1,34 @@
-import React from "react";
-import "./VideoPlayer.css";
-import ReactPlayer from "react-player/lazy";
-import Container from "react-bootstrap/Container";
+import React, { useState } from 'react';
+import './VideoPlayer.css';
+import ReactPlayer from 'react-player/lazy';
+import Container from 'react-bootstrap/Container';
 
 // <video id="background-video" loop autoPlay>
 //   <source src="assets/caledonia_sta_final.mp4" type="video/mp4" />
 //   Your browser does not support the video tag.
 // </video>
 function VideoPlayer({ url }) {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
   return (
     <Container className="video-container">
-      <ReactPlayer className="video-player" url={url} />
+      <ReactPlayer
+        className="video-player"
+        url={url}
+        onReady={() => setVideoLoaded(true)}
+        style={{
+          animation: videoLoaded ? 'videoFadeIn 1.5s 0.5s ease forwards' : '',
+        }}
+      />
+
+      <div
+        className="loading-text"
+        style={{
+          animation: videoLoaded ? 'loadingTextFadeOut 0.5s forwards' : '',
+        }}
+      >
+        Loading Video...
+      </div>
     </Container>
   );
 }
