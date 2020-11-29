6250183071a859138138fcde03bcce81ffe7da5c
Rebecca Still <beckystill@hotmail.com>
2020-11-29 20:30:07 +0000

    additional styling for popup

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -4,12 +4,10 @@ import Button from "react-bootstrap/Button";
 
 function Popup({ closePopup, showPopup, text }) {
   return (
-    <div className="popup">
-      <div className="popupInner">
+      <div className="popup-container">
         <h1>{text}</h1>
         <Button onClick={() => closePopup(showPopup)}>Back</Button>
       </div>
-    </div>
   );
 }
 
