666255eb8048abb2995ec109145036be282b743c
Rebecca Still <beckystill@hotmail.com>
2020-11-30 22:37:51 +0000

    responsive popups and tidy up app.js

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
   const [openWindow, setOpenWindow] = useState(null);
   const [showPasswordEntry, setShowPasswordEntry] = useState(true); //<---- just set this to false until we go live
   // const [currentDate, setCurrentDate] = useState("02 Dec 2020");
-  const [currentDate] = useState(moment().format("D MMM YYYY"));
-  // const [currentDate] = useState(moment().format("D MMM YYYY")); // const currentDate = moment().format("D MMM YYYY"); <---- set to 24 Dec 2020 for development
+  const [currentDate] = useState(moment().format("D MMM YYYY")); // <---- set to 24 Dec 2020 for development
   const allDays = config.days;
   const popupMessage =
     "You will need to wait for that day!";
--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -4,10 +4,12 @@ import Button from "react-bootstrap/Button";
 
 function Popup({ closePopup, showPopup, text }) {
   return (
-      <div className="popup-container">
+    <div className="popup-container">
+      <div className="popup">
         <h1>{text}</h1>
         <Button onClick={() => closePopup(showPopup)}>Back</Button>
       </div>
+    </div>
   );
 }
 
