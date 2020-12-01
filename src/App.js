5a012d8df3b612b885e3f189a1ea0e95ad1a471f
Rebecca Still <beckystill@hotmail.com>
2020-12-01 19:40:45 +0000

    set state for currentDate and passwordEntry

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ import Popup from "./components/Popup/Popup";
 function App() {
   const [page, setPage] = useState("calendar-page");
   const [openWindow, setOpenWindow] = useState(null);
-  const [showPasswordEntry, setShowPasswordEntry] = useState(false); //<---- just set this to false until we go live
-  const [currentDate, setCurrentDate] = useState("02 Dec 2020");
-  // const [currentDate] = useState(moment().format("D MMM YYYY")); // <---- set to 24 Dec 2020 for development
+  const [showPasswordEntry, setShowPasswordEntry] = useState(true); //<---- just set this to false until we go live
+  // const [currentDate, setCurrentDate] = useState("02 Dec 2020");
+  const [currentDate] = useState(moment().format("D MMM YYYY")); // <---- set to 24 Dec 2020 for development
   const allDays = config.days;
   const popupMessage =
     "You will need to wait for that day!";
