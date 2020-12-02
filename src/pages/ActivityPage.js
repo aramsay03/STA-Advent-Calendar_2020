0e6aeb26dfe050a1f46c81effb6e3bb63a6fb39e
aramsay03 <alan@aramsay.co.uk>
2020-12-02 12:00:14 +0000

    Added ability to add small amount of test below video player

--- src/pages/ActivityPage.js
+++ src/pages/ActivityPage.js
@@ -25,7 +25,7 @@ function ActivityPage({ setPage, openWindow, setOpenWindow, currentDate }) {
 
   function activityComponent() {
     if (openWindow.component === "VideoPlayer") {
-      return <VideoPlayer url={openWindow.urls[0]} />;
+      return <VideoPlayer url={openWindow.urls[0]} instructions={openWindow.instructions} />;
     } else if (openWindow.component === "CarouselComponent") {
       return <CarouselComponent day={openWindow}/>;
     } else if (openWindow.component === "PhotoChallengeComponent") {
