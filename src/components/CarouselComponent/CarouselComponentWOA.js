08afb5667d7f090aceac8a13cfe4fec05b80c37c
Rebecca Still <beckystill@hotmail.com>
2020-12-16 11:04:10 +0000

    update Wall of Awesome

--- src/components/CarouselComponent/CarouselComponentWOA.js
+++ src/components/CarouselComponent/CarouselComponentWOA.js
@@ -3,43 +3,92 @@ import Carousel from "react-bootstrap/Carousel";
 import Col from "react-bootstrap/Col";
 
 function CarouselComponentWOA() {
-
   const winners = {
-                    "competitions": [
-                      {
-                        "#": 1,
-                        "header": "Wall of Awesome",
-                        "subheader": "STA's First Ever Pub Quiz Night",
-                        "image": "./assets/STAPubQuizNight.png",
-                        "imageDescription": "Group picture of particpants on zoom call"
-                      },
-                      {
-                        "#": 2,
-                        "header": "STA Pub Quiz Night Winner",
-                        "subheader": "Christopher McLellan",
-                        "image": "./assets/ChristopherMcLellan.png",
-                        "imageDescription": "Picture of the winner Christopher McLellan"
-                      }
-                    ]
-                  }
+    competitions: [
+      {
+        "#": 1,
+        header: "STA's First Ever Pub Quiz Night",
+        subheader: "Obligatory Group Zoom Photo!",
+        image: "./assets/wallOfAwesome/STAPubQuizNight.png",
+        imageDescription: "Group picture of particpants on zoom call",
+      },
+      {
+        "#": 2,
+        header: "STA Pub Quiz Night 2020",
+        subheader: "Winner - Christopher M",
+        image: "./assets/wallOfAwesome/ChristopherMcLellan.png",
+        imageDescription: "Picture of the winner Christopher McLellan",
+      },
+      {
+        "#": 3,
+        header: "Best Christmas Jumper 2020",
+        subheader: "Winner - Maud",
+        image: "./assets/wallOfAwesome/Maud.jpg",
+        imageDescription:
+          "Picture of winner - Maud the mannequin in her christmas jumper",
+      },
+      {
+        "#": 4,
+        header: "Best Christmas Jumper 2020",
+        subheader: "Runner Up - Alistair F",
+        image: "./assets/wallOfAwesome/Alistair.jpg",
+        imageDescription:
+          "Picture of runner up - Alistair wearing his christmas jumper",
+      },
+      {
+        "#": 5,
+        header: "Best Christmas Jumper 2020",
+        subheader: "Runner Up - Kirsty M",
+        image: "./assets/wallOfAwesome/Kirstyjumper.jpg",
+        imageDescription:
+          "Picture of runner up - Kirsty wearing her christmas jumper",
+      },
+      {
+        "#": 6,
+        header: "Winter Photography Challenge 2020",
+        subheader: "Submitted by Robin W",
+        image: "./assets/wallOfAwesome/Robin1.jpg",
+        imageDescription: "Picture of Scottish landscape",
+      },
+      {
+        "#": 7,
+        header: "Winter Photography Challenge 2020",
+        subheader: "Submitted by Robin W",
+        image: "./assets/wallOfAwesome/Robin2.JPG",
+        imageDescription: "Picture of Scottish landscape",
+      },
+      {
+        "#": 8,
+        header: "Winter Photography Challenge 2020",
+        subheader: "Submitted by Rhona H",
+        image: "./assets/wallOfAwesome/Rhona.jpg",
+        imageDescription: "Picture of snowman wearing a mask",
+      },
+    ],
+  };
 
   const carouselPagesImages = winners.competitions.map((competition, index) => {
-
     return (
-      <Carousel.Item  className="woa-carousel" key={index}>
-        <h1>{competition.header}</h1>
-        <h2>{competition.subheader}</h2>
-        <img className="wall-of-awesome-image" src={competition.image} alt={competition.imageDescription}/>
+      <Carousel.Item className="woa-carousel" key={index}>
+        <div className="woa-text">
+          <h1>
+            <b>Wall of Awesome</b>
+          </h1>
+          <h2>{competition.header}</h2>
+          <h3>{competition.subheader}</h3>
+        </div>
+        <img
+          className="wall-of-awesome-image"
+          src={competition.image}
+          alt={competition.imageDescription}
+        />
       </Carousel.Item>
     );
-
   });
 
   return (
     <Col>
-      <Carousel interval={null}>
-        {carouselPagesImages}
-      </Carousel>
+      <Carousel interval={null}>{carouselPagesImages}</Carousel>
     </Col>
   );
 }
