41dc1cc44ac5ab52f00d222565a8de7003febeb7
aramsay03 <alan@aramsay.co.uk>
2020-12-23 16:32:44 +0000

    Day24 podcast component added

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ function App() {
   const [openWindow, setOpenWindow] = useState(null);
 
 // For Production
-const [showPasswordEntry, setShowPasswordEntry] = useState(true);
-const [currentDate] = useState(moment().format("YYYYMMDD"));
+// const [showPasswordEntry, setShowPasswordEntry] = useState(true);
+// const [currentDate] = useState(moment().format("YYYYMMDD"));
 
 
 // For Development
-// const [showPasswordEntry, setShowPasswordEntry] = useState(false);
-// const [currentDate] = useState("20201224");
+const [showPasswordEntry, setShowPasswordEntry] = useState(false);
+const [currentDate] = useState("20201224");
 
   const allDays = config.days;
   const popupMessage = "You will need to wait for that day!";
